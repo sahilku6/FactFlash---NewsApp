4040935afa05da474a02278efcea4fe71fe63ca4
Sahil Kumar <sahilkumarlpu1@gmail.com>
2025-09-23 14:41:17 +0530

    checking looping

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description} = this.props;
+        let {title, description,imageUrl} = this.props;
         return (
             <div>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="..." className="card-img-top" alt="..."/>
+                    <img src={imageUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <a href="/newsDetails" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{description}</p>
+                             <a href="/newsUrl" className="btn btn-sm btn-primary">Read More</a>
                         </div>
                 </div>
             </div>
